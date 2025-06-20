ed696a92efe21439792e9ee1ca0a065eae5a239d
Ahmad Nabil Avila <nabilavila050@gmail.com>
2025-06-20 15:00:06 +0800

    Menu Manajemen Buku

--- Perpus.cpp
+++ Perpus.cpp
@@ -1,14 +1,17 @@
-
 #include <iostream>
 #include <string>
 using namespace std;
 
+
+void manajemenBuku();
 void menuUtama(){
     int pilihan;
     do{
-        cout<<"+========================================+"<<end1;
+        cout<<"+========================================+"<<endl;
         cout<<"\n    SISTEM PERPUSTAKAAN    "<<endl;
-        cout<<"+========================================+"<<end1;
+        cout<<"+========================================+"<<endl;
+        cout<<""<<endl;
+
         cout<<"1. Manajemen Buku"<<endl;
         cout<<"2. Manajemen Anggota"<<endl;
         cout<<"3. Pinjam Buku"<<endl;
@@ -21,6 +24,8 @@ void menuUtama(){
 
         switch(pilihan){
             case 1:
+                manajemenBuku();
+                system ("cls");
                 break;
             case 2:
                 break;
@@ -34,4 +39,51 @@ void menuUtama(){
                 break;
         }
     }while(pilihan != 6);
+}
+
+void manajemenBuku(){
+
+    int pilihan;
+
+    do{
+        cout<<"+========================================+"<<endl;
+        cout<<"\n    MENU MANAJEMEN BUKU     "<<endl;
+        cout<<"+========================================+"<<endl;
+        cout<<""<<endl;
+
+        cout<<"1. Tambah Buku"<<endl;
+        cout<<"2. Tampilkan Daftar Buku"<<endl;
+        cout<<"3. Hapus Buku"<<endl;
+        cout<<"4. Kembali ke menu Utama"<<endl;
+        cout<<"Pilih: ";
+        cin>>pilihan;
+        system("cls");
+
+        switch(pilihan){
+
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                cout<<"Kembali ke menu Utama"<<endl;
+                break;
+
+            default:
+
+                cout<<"Pilihan tidak valid";
+
+        }
+
+    }while(pilihan != 4);
+
 }
